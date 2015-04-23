bf5ebf486a50d73ea96942155b5c51d80ea93f35
Frans Slothouber <rfsber@xs4all.nl>
2015-04-23 13:12:10 +0200

    fix for failing make install

---
+++ Test/Build/test_build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# TODO Fetch from website
+version=4.99.43
+tarball=../../robodoc-4.99.43.tar.gz
+builddir=/tmp/robo
+
+echo $tarball
+
+# Drop out on the first error we encounter
+set -e
+
+rm -rf $builddir
+mkdir $builddir
+cp $tarball $builddir/
+cd $builddir
+tar -zxf robodoc*.gz
+cd robodoc-$version
+./configure
+make
+build_version=`./Source/robodoc --version`
+
+echo "Build was successful."
+echo "Executable reports to be $build_version"
+
+# vi: spell spl=en
