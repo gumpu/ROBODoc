941342a27ab78af62c2c44b386a7f5d15c387ee8
Frans Slothouber <frans.slothouber@gmail.com>
2021-01-30 21:15:57 +0100

    Refactors build_robodoc.sh

--- build_robodoc.sh
+++ build_robodoc.sh
@@ -3,15 +3,18 @@
 # Use this file to build ROBODoc when you used git clone to
 # get the ROBODoc sources
 #
+# You can also use it to recreate all autoconfig files from
+# the official source release.
 #
-# If a ./do.sh under cygwin gives
+# If a ./build_robodoc.sh under cygwin gives
 #  : command not found
 #  : command not found
 #  : command not foundclocal
 #
 # Then this file has wrong line-endings (cr/lf).
 # To fix it unzip the archive under cygwin with the
-# option -a, so
+# option -a, so:
+#
 #   unzip -a robodoc-xx-xx-xx.zip
 #
 
@@ -25,20 +28,20 @@ rm -f mkinstalldirs
 rm -f missing makefile
 rm -f configure config.status
 
-# CFLAGS="-g -Wall -DDMALLOC -DMALLOC_FUNC_CHECK" LDFLAGS="-ldmalloc" ./configure
-# CFLAGS="-g -Wall -O0 -std=gnu99 -pedantic -Wshadow -Wbad-function-cast -Wconversion -Wredundant-decls" ./configure
 # autoreconf calls all needed autotools
 # -f consider all autotool files obsolete
 # -i copy missing auxiliary files
 automake
 autoreconf -f -i
 
-# distcheck creates all distribution packages and does some sanity checks on it
-#
+# CFLAGS="-g -Wall -DDMALLOC -DMALLOC_FUNC_CHECK" LDFLAGS="-ldmalloc" ./configure
+# CFLAGS="-g -Wall -O0 -std=gnu99 -pedantic -Wshadow -Wbad-function-cast -Wconversion -Wredundant-decls" ./configure
 # Adding -Werror makes the test for snprintf fail!  Bug in autoconfig?
 # CFLAGS="-g -Wall -Werror -O0 -Wshadow -Wredundant-decls" ./configure
 #
 CFLAGS="-g -Wall -O0 -Wshadow -Wredundant-decls" ./configure
+
+# distcheck creates all distribution packages and does some sanity checks on it
 make distcheck
 # Make clean and recompile
 make clean
